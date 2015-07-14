e186f61ff9b53705d269febc84209909bd6bab88
alxcoh <alxcoh@gmail.com>
2015-07-14 11:00:37 -0400

    .

--- static/js/shape_task.js
+++ static/js/shape_task.js
@@ -4,27 +4,37 @@ var img_attr_choies = [ ['Rectangle', 'Triangle'],
                         ['Dot', 'NoDot'] ]
 
 var possible_questions_full = [  [
-                              ['Is it a rectangle?', [0, 0]],
-                              ['Is it a triangle?', [0, 1]]
-                            ],
+                                    ['Is it a rectangle?', [0, 0]],
+                                    ['Is it a triangle?', [0, 1]]
+                                  ],
 
-                            [
+                                  [
+                                    ['Does it have a black outline?', [1, 0]],
+                                    ['Does it have a red outline?', [1, 1]]
+                                  ],
+
+                                  [
+                                    ['Does it have a green fill?', [2, 0]],
+                                    ['Does is have a gray fill?', [2, 1]]
+                                  ],
+
+                                  [
+                                    ['Does it have a dot?', [3, 0]],
+                                    ['Does it have a dot?', [3, 0]],
+                                  ]
+                              ];
+
+var good_questions_full =  [  ['Is it a rectangle?', [0, 0]],
+                              ['Is it a triangle?', [0, 1]],
                               ['Does it have a black outline?', [1, 0]],
-                              ['Does it have a red outline?', [1, 1]]
-                            ],
+                              ['Does it have a red outline?', [1, 1]],
+                              ['Does is have a gray fill?', [2, 1]],
+                              ['Does it have a dot?', [3, 0]] ];
 
-                            [
-                              ['Does it have a green fill?', [2, 0]],
-                              ['Does is have a gray fill?', [2, 1]]
-                            ],
+var possible_questions = possible_questions_full.slice();
+var good_questions = good_questions_full.slice();
 
-                            [
-                              ['Does it have a dot?', [3, 0]],
-                              ['Does it have a dot?', [3, 0]],
-                            ]
-                          ];
-var possible_questions = possible_questions_full.slice()
-var shape_imgs = []
+var shape_imgs = [];
 var shape_choice = '';
 var knowledge_arr = [];
 var iterations = 0;
@@ -67,11 +77,22 @@ function load_img_names() {
   return shuffle(shape_imgs);
 }
 
+function add_option(q_num, q) {
+  $("#answers").append(
+    '<div class="radio-question" id = "radio-question-1">' +
+      '<input type="radio" name="q1" value="a" id="q' + q_num.toString() + '">' +
+      '<label style="margin:0.5em" for="q' + q_num.toString() + '">' + q + '</label>' +
+    '</div>'
+  );
+
+}
+
 function start_shapegame() {
+  good_questions = shuffle(good_questions_full);
   shape_imgs = load_img_names();
   shape_choice = base2str(rand_num_incl(0, 15));
   possible_questions = possible_questions_full.slice();
-  give_question_options();
+  give_question_options();)
 }
 
 function let_them_choose() {
@@ -98,27 +119,11 @@ function shape_chosen(obj) {
 
 function load_shape_imgs() {
   for(var i = 0; i < 16; i++) {
-
-    /* removes inconsistent shapes
-    var consistent_with_knowledge = true;
-    for(var j = 0; j < knowledge_arr.length; j++) {
-      k = knowledge_arr[j];
-      alert("This piece of knowledge: " + k);
-      if(base2str(i).charAt(k[0]) != k[1]) {
-        consistent_with_knowledge = false;
-      }
-    }
-    alert("I decided it was " + (consistent_with_knowledge ? 'consistent' : 'inconsistent') +
-                      ", because it had base2str, knowledge: " +
-                      base2str(i) + " : " + knowledge_arr);
-
-    */
-
     $("#shape" + (i+1).toString()).html(shape_imgs[i]);
   }
 }
 
-function give_question_options() {
+function give_question_options_old() {
   if(possible_questions.length == 0) {
     let_them_choose();
     return;
@@ -155,36 +160,6 @@ function give_question_options() {
   $("#q1").next('label').attr('knowledge_val', question2[1].join());
 }
 
-var choicecomplete = function() {
-	if($("#submit-button").html() == "Submit") {
-		var choice = $("input[name=q1]:checked").next('label').html();
-    var knowledge_val = $("input[name=q1]:checked").next('label').attr('knowledge_val').split(',');
-
-    for(var i = 0; i < possible_questions.length; i++) {
-      if(possible_questions[i][0][1][0] == knowledge_val[0]) {
-        possible_questions = removeArrValue(possible_questions, i);
-        break;
-      }
-    }
-
-    var true_val = shape_choice.charAt(knowledge_val[0]);
-    console.log(knowledge_val[0] + ':' + true_val + ':' + knowledge_val[1])
-    var resp = true_val == knowledge_val[1] ? 'Yes' : 'No';
-    knowledge_arr.push([knowledge_val[0], true_val]);
-
-		$("#prev-questions").css("margin-left", "0px");
-		$("#answer").html('<h1 style="margin-top: 0">' + resp + '</h1>');
-		$("#answer").fadeTo('slow', 1.0);
-		$("#submit-button").html("Next");
-		$("#submit-button").removeClass('btn-success');
-		$("#submit-button").addClass("btn-primary");
-    $("#answers").remove();
-    $("#form-label").html(choice);
-	}
-
-	else {
-		iterations++;
-		give_question_options();
-	}
+function give_question_options() {
 
 }
